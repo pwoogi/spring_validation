01a7c86ee1d792d90b22daef687578e791ba177e
pwoogi <chrishw1029@gmail.com>
2022-11-07 20:15:23 +0900

    Exception 전역처리 기본 복습

---
+++ src/main/java/com/example/validation/contorller/RestApiController.java
@@ -0,0 +1,42 @@
+package com.example.validation.contorller;
+
+
+import com.example.validation.dto.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/api-v2")
+public class RestApiController {
+
+    @GetMapping("/member")
+    public Member get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+        Member member = new Member();
+        member.setName(name);
+        member.setAge(age);
+
+        int a = 10+age;
+
+
+        return member;
+    }
+
+    @PostMapping("/")
+    public Member post(@Valid @RequestBody Member member){
+        System.out.println(member);
+        return member;
+    }
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e){
+
+        System.out.println("api controller");
+
+        // controller안에 직접 @ExcpetionHandler를 지정해주면 전역처리되기전에 적용한 controller에서 처리가 된다는 점
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+    }
+}
