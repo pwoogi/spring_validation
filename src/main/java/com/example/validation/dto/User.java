b116d340d5fd0593a02b03d730f88fdfcfa14e0c
pwoogi <chrishw1029@gmail.com>
2022-11-07 17:56:07 +0900

    @AssertTrue 활용해보기

--- src/main/java/com/example/validation/dto/User.java
+++ src/main/java/com/example/validation/dto/User.java
@@ -1,18 +1,36 @@
 package com.example.validation.dto;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Pattern;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class User {
 
+    @NotBlank
     private String name;
+
+    @Max(value = 90)
     private int age;
 
     @Email
     private String email;
 
-    @Pattern(regexp = "(\\d{3})(\\d{3,4})(\\d{4})", message = "핸드폰 번호의 양식과 맞지 않습니다. xxx-xxxx-xxxx")
+    @JsonProperty("phone_number")
+    @Pattern(regexp = "^\\d{3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호의 양식과 맞지 않습니다. xxx-xxxx-xxxx")
     private String phoneNumber;
+
+    @Size(min = 6, max = 6)
+    private String reqYearMonth;
+
+    public String getReqYearMonth() {
+        return reqYearMonth;
+    }
+
+    public void setReqYearMonth(String reqYearMonth) {
+        this.reqYearMonth = reqYearMonth;
+    }
 
     public String getName() {
         return name;
@@ -53,6 +71,20 @@ public class User {
                 ", age=" + age +
                 ", email='" + email + '\'' +
                 ", phoneNumber='" + phoneNumber + '\'' +
+                ", reqYearMonth='" + reqYearMonth + '\'' +
                 '}';
+    }
+
+    @AssertTrue
+    public boolean isReqYearMonthValidation(){
+
+        try {
+            LocalDate localDate = LocalDate.parse(getReqYearMonth()+"01", DateTimeFormatter.ofPattern("yyyyMMdd"));
+
+        }catch (Exception e){
+            return false;
+        }
+        return true;
+
     }
 }
